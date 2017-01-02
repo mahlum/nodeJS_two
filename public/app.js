e2fe737079a7ddc46826c0b4fd96ce801594c9aa
mahlum <mahlum@gmx.de>
2017-01-02 22:56:57 +0100

    commi

--- public/app.js
+++ public/app.js
@@ -1,1 +1,17 @@
 var fs = require('fs');
+
+fs.writeFileSync("corn.txt", "Corn is good, corn ist life");
+console.log(fs.readFileSync("corn.txt").toString());
+console.log("Done");
+
+var path = require('path');
+var websiteHome = "Desktop/tip/this/is/a/website.html";
+var websiteAbout = "Desktop\good\night\or\what";
+
+console.log(path.normalize(websiteHome));
+console.log(path.dirname(websiteHome));
+console.log(path.basename(websiteHome));
+console.log(path.extname(websiteHome));
+
+console.log(__dirname);
+console.log(__filename);
