c1fcb706f5742a2eec820ebbcccd7ccd091262fe
mahlum <mahlum@gmx.de>
2017-01-03 22:35:22 +0100

    Install connect and use it

---
+++ public/server_connect.js
@@ -0,0 +1,26 @@
+var connect = require('connect');
+var http = require('http');
+
+var app = connect();
+
+function doFirst(request, response, next){
+	console.log("Someone request ...");
+	next();
+}
+
+function doSecond(request, response, next){
+	console.log("Someone request second ...");
+	next();
+}
+
+function profile(request, response){
+	console.log("User entered profile");
+}
+
+app.use(doFirst);
+app.use(doSecond);
+
+app.use('/profile', profile);
+
+http.createServer(app).listen(8888);
+console.log("Server is running...");
